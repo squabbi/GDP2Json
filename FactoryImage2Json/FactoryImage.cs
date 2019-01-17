9d21f214f510bf7ba392537f849532334ca9f094
Squabbi <squabbi@users.noreply.github.com>
2019-01-17 17:07:38 +1100

    Add project files.

---
+++ FactoryImage2Json/FactoryImage.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryImage2Json
+{
+    class FactoryImage
+    {
+        private readonly string codename;
+        private readonly string androidVersion;
+        private readonly string androidBuild;
+        private readonly string build;
+        private readonly string date;
+        private readonly string link;
+        private readonly string checksum;
+
+        public FactoryImage(string codename, string androidVersion, string androidBuild, string build, string date, string link, string checksum)
+        {
+            this.codename = codename ?? throw new ArgumentNullException(nameof(codename));
+            this.androidVersion = androidVersion ?? throw new ArgumentNullException(nameof(androidVersion));
+            this.androidBuild = androidBuild ?? throw new ArgumentNullException(nameof(androidBuild));
+            this.build = build ?? throw new ArgumentNullException(nameof(build));
+            this.date = date ?? throw new ArgumentNullException(nameof(date));
+            this.link = link ?? throw new ArgumentNullException(nameof(link));
+            this.checksum = checksum ?? throw new ArgumentNullException(nameof(checksum));
+        }
+
+        public string Codename
+        {
+            get { return codename; }
+        }
+
+        public string AndroidVersion
+        {
+            get { return androidVersion; }
+        }
+
+        public string AndroidBuild
+        {
+            get { return androidBuild; }
+        }
+
+        public string Build
+        {
+            get { return build; }
+        }
+
+        public string Date
+        {
+            get { return date; }
+        }
+
+        public string Link
+        {
+            get { return link; }
+        }
+
+        public string Checksum
+        {
+            get { return checksum; }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Android {0}, {1} - {2}", androidVersion, date, androidBuild);
+        }
+    }
+}
---
+++ FactoryImage2Json/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.IO;
+
+using Newtonsoft.Json;
+
+using AngleSharp;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+namespace FactoryImage2Json
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Task<List<FactoryImage>> factoryImages;
+
+        private async void LoadTableBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(tableNoTb.Text, out int tableNo))
+            {
+                factoryImages = LoadFactoryImages(tableNo);
+                // Load into Data Grid
+                factoryImagesDg.ItemsSource = await factoryImages;
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number...");
+                tableNoTb.Clear();
+            }
+        }
+
+        private async Task<List<FactoryImage>> LoadFactoryImages(int tableNo)
+        {
+            List<FactoryImage> factoryImages = new List<FactoryImage>();
+
+            // AngleSharp, rip table contents
+            var config = Configuration.Default.WithDefaultLoader();
+            var source = "https://developers.google.com/android/images";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(source);
+
+            // Locate all tables in the site (containing the factory images)
+            var tables = document.QuerySelectorAll("table");
+            // Locate the desired table
+            var table = (IHtmlTableElement)tables[tableNo];
+            var tableContents = table.Rows;
+            // Go through each row after the first one (title row)
+            for (int i = 1; i < tableContents.Length; i++)
+            {
+                var rowChildren = tableContents[i].Children;
+                // Child 1 - Version, child 2 - Link (href), child 3 - Checksum
+                string version = rowChildren[0].TextContent;
+                string link = ((IHtmlAnchorElement)rowChildren[1].FirstChild).Href.ToString();
+                string checksum = rowChildren[2].TextContent;
+
+                // Split up version and link by '/' and '-' to get individual elements
+                string[] versionSplit = version.Split(' ');
+                string[] linkSplit = link.Split(new char[] { '/', '-' });
+
+                // Create the Factory Image and add it to the list of factory images
+                factoryImages.Add(new FactoryImage(linkSplit[6], versionSplit[0], linkSplit[7].ToUpper(), linkSplit[9].Substring(0,8), version, link, checksum));
+            }
+
+            return factoryImages;
+        }
+
+        private async Task<List<FactoryImage>> LoadAllFactoryImages()
+        {
+            List<FactoryImage> factoryImages = new List<FactoryImage>();
+
+            // AngleSharp, rip table contents
+            var config = Configuration.Default.WithDefaultLoader();
+            var source = "https://developers.google.com/android/images";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(source);
+
+            // Locate all tables in the site (containing the factory images)
+            var tables = document.QuerySelectorAll("table");
+            // Go through all tables
+            foreach (IHtmlTableElement table in tables)
+            {
+                var tableContents = table.Rows;
+                // Go through each row after the first one (title row)
+                for (int i = 1; i < tableContents.Length; i++)
+                {
+                    var rowChildren = tableContents[i].Children;
+                    // Child 1 - Version, child 2 - Link (href), child 3 - Checksum
+                    string version = rowChildren[0].TextContent;
+                    string link = ((IHtmlAnchorElement)rowChildren[1].FirstChild).Href.ToString();
+                    string checksum = rowChildren[2].TextContent;
+
+                    // Split up version and link by '/' and '-' to get individual elements
+                    string[] versionSplit = version.Split(' ');
+                    string[] linkSplit = link.Split(new char[] { '/', '-' });
+
+                    // Create the Factory Image and add it to the list of factory images
+                    factoryImages.Add(new FactoryImage(linkSplit[6], versionSplit[0], linkSplit[7].ToUpper(), linkSplit[9].Substring(0, 8), version, link, checksum));
+                }
+            }
+
+            return factoryImages;
+        }
+
+        private void OpenSiteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://developers.google.com/android/images");
+        }
+
+        private void ExportBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = filenameTb.Text;
+
+            // serialize JSON directly to a file
+            using (StreamWriter file = File.CreateText(filename))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, factoryImages.Result);
+            }
+        }
+
+        private void OpenFolderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Environment.CurrentDirectory);
+        }
+
+        private async void LoadAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            factoryImages = LoadAllFactoryImages();
+            // Load into Data Grid
+            factoryImagesDg.ItemsSource = await factoryImages;
+        }
+
+        private void ClipboardBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(factoryImages.Result);
+            Clipboard.SetText(json);
+        }
+    }
+}
