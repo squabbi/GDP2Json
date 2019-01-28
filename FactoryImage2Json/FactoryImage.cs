c92ee27ee6267b9e5f9a6ba3f85d826daa2eb3c2
Squabbi <squabbi@users.noreply.github.com>
2019-01-28 16:58:41 +1100

    Updated FactoryImage class

--- FactoryImage2Json/FactoryImage.cs
+++ FactoryImage2Json/FactoryImage.cs
@@ -1,70 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FactoryImage2Json
 {
     class FactoryImage
     {
-        private readonly string codename;
-        private readonly string androidVersion;
-        private readonly string androidBuild;
-        private readonly string build;
-        private readonly string date;
-        private readonly string link;
-        private readonly string checksum;
-
         public FactoryImage(string codename, string androidVersion, string androidBuild, string build, string date, string link, string checksum)
         {
-            this.codename = codename ?? throw new ArgumentNullException(nameof(codename));
-            this.androidVersion = androidVersion ?? throw new ArgumentNullException(nameof(androidVersion));
-            this.androidBuild = androidBuild ?? throw new ArgumentNullException(nameof(androidBuild));
-            this.build = build ?? throw new ArgumentNullException(nameof(build));
-            this.date = date ?? throw new ArgumentNullException(nameof(date));
-            this.link = link ?? throw new ArgumentNullException(nameof(link));
-            this.checksum = checksum ?? throw new ArgumentNullException(nameof(checksum));
+            this.Codename = codename ?? throw new ArgumentNullException(nameof(codename));
+            this.AndroidVersion = androidVersion ?? throw new ArgumentNullException(nameof(androidVersion));
+            this.AndroidBuild = androidBuild ?? throw new ArgumentNullException(nameof(androidBuild));
+            this.Build = build ?? throw new ArgumentNullException(nameof(build));
+            this.Date = date ?? throw new ArgumentNullException(nameof(date));
+            this.Link = link ?? throw new ArgumentNullException(nameof(link));
+            this.Checksum = checksum ?? throw new ArgumentNullException(nameof(checksum));
         }
 
-        public string Codename
-        {
-            get { return codename; }
-        }
+        public string Codename { get; }
 
-        public string AndroidVersion
-        {
-            get { return androidVersion; }
-        }
+        public string AndroidVersion { get; }
 
-        public string AndroidBuild
-        {
-            get { return androidBuild; }
-        }
+        public string AndroidBuild { get; }
 
-        public string Build
-        {
-            get { return build; }
-        }
+        public string Build { get; }
 
-        public string Date
-        {
-            get { return date; }
-        }
+        public string Date { get; }
 
-        public string Link
-        {
-            get { return link; }
-        }
+        public string Link { get; }
 
-        public string Checksum
-        {
-            get { return checksum; }
-        }
+        public string Checksum { get; }
 
         public override string ToString()
         {
-            return String.Format("Android {0}, {1} - {2}", androidVersion, date, androidBuild);
+            return String.Format("Android {0}, {1} - {2}", AndroidVersion, Date, AndroidBuild);
         }
     }
 }
